9a03a0c0bc49b8f89bf6312fe411a87935413924
bastazini <bastazini.vinicius@gmail.com>
2018-03-02 12:27:00 +0100

    Update Pairwise comparison Permanova, based on distance matrices.R

--- Pairwise comparison Permanova, based on distance matrices.R
+++ Pairwise comparison Permanova, based on distance matrices.R
@@ -1,4 +1,4 @@
-#Function for performing pairwise comparisons with the “adonis”  (package vegan) direct on distance matrices
+#Function for performing pairwise comparisons using the function “adonis”  (R package vegan) direct on distance matrices
 #From: Dias, R. A., Gianuca, A. T., Vizentin-Bugoni, J., Gonçalves, M. S. S., Bencke, G. A., & Bastazini, V. A. Livestock disturbance in Brazilian grasslands influences avian species diversity via turnover. Biodiversity and Conservation, 1-18.
 #see msuplmentary material 2 for further details
 ###############################
