c9848a6d6760d0ec3ce6cf74e8789a90d5abe2f0
bastazini <bastazini.vinicius@gmail.com>
2023-09-26 14:34:37 +0100

    Update distance decay.R

--- distance decay.R
+++ distance decay.R
@@ -1,5 +1,5 @@
 ###################### 
-#code for fitting multiple models (i.e., intercept only, linear, log, exponential and assintotic model) for distance decay curves (i.e., the  decay of  ecological community similarity with geographic distance)
+#code for fitting multiple models (i.e., intercept only, linear, log, exponential and asymptotic model) for distance decay curves (i.e., the  decay of  ecological community similarity with geographic distance)
 #####################
 
 install.packages("bbmle")
@@ -16,18 +16,19 @@ null=lm(similarity~1)
 linear=lm(similarity~distance) 
 
 # exponential
-exponential=nls(similarity~a+b*log(distance),start = list(a = coefficients(linear)[1], b = coefficients(linear)[2]))
-# assintotic
-assintotic=nls(similarity~a+b/(distance),start = list(a = coefficients(linear)[1], b = coefficients(linear)[2])) 
+exponential =nls(similarity ~ a * exp(b * distance), start = list(a = coefficients(linear)[1], b = coefficients(linear)[2]))
+exponential_log= =nls(similarity~a+b*log(distance),start = list(a = coefficients(linear)[1], b = coefficients(linear)[2]))
+# asymptotic
+asymptotic=nls(similarity~a+b/(distance),start = list(a = coefficients(linear)[1], b = coefficients(linear)[2])) 
 
 
-AICctab(null,linear,exponential,assintotic,nobs=100,weights = TRUE, delta = TRUE, base = TRUE)
+AICctab(null,linear,exponential,asymptotic,nobs=lentgh(x),weights = TRUE, delta = TRUE, base = TRUE)
 
 #Plot data amd fitted models
 plot(similarity~distance,xlab="Distance", ylab="Similarity")
 abline(null,col="green")
 abline(linear,col="red")
 lines(coefficients(exponential)[1]+(coefficients(exponential)[2])*log(1:max(distance)))
-lines(coefficients(assintotic)[1]+(coefficients(assintotic)[2])/(1:max(distance)), col="blue")
+lines(coefficients(asymptotic)[1]+(coefficients(asymptotic)[2])/(1:max(distance)), col="blue")
 
 
