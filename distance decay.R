d668d9ff445fead14b27c91e7499267d621be4e6
bastazini <bastazini.vinicius@gmail.com>
2023-11-04 09:26:52 +0000

    Update distance decay.R

--- distance decay.R
+++ distance decay.R
@@ -6,29 +6,29 @@ install.packages("bbmle")
 require(bbmle)
 
 #data vectors  representing site similarity and geographic distances between sites
-#similarity
-#distance
+#y=similarity
+#x=distance
 
 #Models
 # null
-null=lm(similarity~1)
+null=lm(y~1)
 # linear
-linear=lm(similarity~distance) 
+linear=lm(y~x) 
 
 # exponential
-exponential =nls(similarity ~ a * exp(b * distance), start = list(a = coefficients(linear)[1], b = coefficients(linear)[2]))
-exponential_log= =nls(similarity~a+b*log(distance),start = list(a = coefficients(linear)[1], b = coefficients(linear)[2]))
+exponential =nls(y ~ a * exp(b * x), start = list(a = coefficients(linear)[1], b = coefficients(linear)[2]))
+exponential_log= =nls(y~a+b*log(x),start = list(a = coefficients(linear)[1], b = coefficients(linear)[2]))
 # asymptotic
-asymptotic=nls(similarity~a+b/(distance),start = list(a = coefficients(linear)[1], b = coefficients(linear)[2])) 
+asymptotic=nls(y~a+b/(x),start = list(a = coefficients(linear)[1], b = coefficients(linear)[2])) 
 
 
 AICctab(null,linear,exponential,asymptotic,nobs=lentgh(x),weights = TRUE, delta = TRUE, base = TRUE)
 
 #Plot data amd fitted models
-plot(similarity~distance,xlab="Distance", ylab="Similarity")
+plot(y~x,xlab="Distance", ylab="Similarity")
 abline(null,col="green")
 abline(linear,col="red")
-lines(coefficients(exponential)[1]+(coefficients(exponential)[2])*log(1:max(distance)))
-lines(coefficients(asymptotic)[1]+(coefficients(asymptotic)[2])/(1:max(distance)), col="blue")
+lines(coefficients(exponential)[1]+(coefficients(exponential)[2])*log(1:max(x)))
+lines(coefficients(asymptotic)[1]+(coefficients(asymptotic)[2])/(1:max(x)), col="blue")
 
 
