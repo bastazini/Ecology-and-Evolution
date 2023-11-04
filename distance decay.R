dd9b5f0f825200e228fe43ed422d696f7167cd60
bastazini <bastazini.vinicius@gmail.com>
2023-11-04 09:30:51 +0000

    Update distance decay.R

--- distance decay.R
+++ distance decay.R
@@ -14,7 +14,6 @@ require(bbmle)
 null=lm(y~1)
 # linear
 linear=lm(y~x) 
-
 # exponential
 exponential =nls(y ~ a * exp(b * x), start = list(a = coefficients(linear)[1], b = coefficients(linear)[2]))
 exponential_log= nls(y~a+b*log(x),start = list(a = coefficients(linear)[1], b = coefficients(linear)[2]))
@@ -22,7 +21,7 @@ exponential_log= nls(y~a+b*log(x),start = list(a = coefficients(linear)[1], b = 
 asymptotic=nls(y~a+b/(x),start = list(a = coefficients(linear)[1], b = coefficients(linear)[2])) 
 
 
-AICctab(null,linear,exponential,asymptotic,nobs=length(x),weights = TRUE, delta = TRUE, base = TRUE)
+AICctab(null,linear,exponential,exponential_log,asymptotic,nobs=length(x),weights = TRUE, delta = TRUE, base = TRUE)
 
 #Plot data amd fitted models
 plot(y~x,xlab="Distance", ylab="Similarity")
